15f1cb5b42c29dabe4ee0fb220afacc1bac3f9d2
amuhirwa <144004199+amuhirwa@users.noreply.github.com>
2024-05-20 22:15:04 +0200

    Add files via upload

--- UniqueInt.py
+++ UniqueInt.py
@@ -1,36 +1,42 @@
 class UniqueInt:
     """Class which contains all the methods used in processing input file and output file."""
-    def ltrim(self, s):
+    @staticmethod
+    def ltrim(s):
         """Remove leading spaces and tabs from the string."""
         i = 0
         while s[i] == ' ' or s[i] == '\t':
             i += 1
         return s[i:]
 
-    def custom_append(self, lst, item):
+    @staticmethod
+    def custom_append(lst, item):
         """Append an item to the list."""
         lst += [item]
         return lst
 
-    def rtrim(self, s):
+    @staticmethod
+    def rtrim(s):
         """Remove trailing spaces, newlines, and tabs from the string."""
         i = 1
         while i <= len(s) and (s[-i] == ' ' or s[-i] == '\n' or s[-i] == '\t'):
             i += 1
         return s[:(len(s) + 1) - i]
 
-    def trim(self, s):
+    @staticmethod
+    def trim(s):
         """Remove both leading and trailing spaces, newlines, and tabs from the string."""
-        return self.rtrim(self.ltrim(s))
+        return UniqueInt.rtrim(UniqueInt.ltrim(s))
 
-    def reverse_string(self, s):
+    @staticmethod
+    def reverse_string(s):
         """Reverse the given string."""
         output = ''
         for i in range(1,  len(s) + 1):
             output += s[-i]
         return output
 
-    def str_to_int(self, s):
+    @staticmethod
+    def str_to_int(s):
         """Convert a string to an integer. Returns False if the string is not a valid integer or exceeds 1023."""
         output = 0
         for i in s:
@@ -47,7 +53,8 @@ class UniqueInt:
             output *= -1
         return output
 
-    def int_to_str(self, i):
+    @staticmethod
+    def int_to_str(i):
         """Convert an integer to a string."""
         output = ''
         negative = 0
@@ -59,10 +66,11 @@ class UniqueInt:
         while i > 0:
             output += chr((i % 10) + ord('0'))
             i //= 10
-        output = self.reverse_string(output)
+        output = UniqueInt.reverse_string(output)
         return ('-' * negative) + output
 
-    def custom_sort(self, array):
+    @staticmethod
+    def custom_sort(array):
         """Sort the array using a custom quicksort implementation."""
         if len(array) <= 1:
             return array
@@ -74,7 +82,7 @@ class UniqueInt:
                 left.append(i)
             else:
                 right.append(i)
-        return self.custom_sort(left) + [pivot] + self.custom_sort(right)
+        return UniqueInt.custom_sort(left) + [pivot] + UniqueInt.custom_sort(right)
 
     # AllNumbers = {}
     # allstrings = []
@@ -83,31 +91,35 @@ class UniqueInt:
     #         AllNumbers[num] = i
     #         allstrings.append(num)
 
-    def processFile(self, input_file, output_file):
+    @staticmethod
+    def processFile(input_file, output_file):
         """
         Read numbers from the input file, process and remove duplicates,
         then write the sorted unique numbers to the output file.
         """
+        if not output_file:
+            output_file = input_file + '_results.txt'
         unique_ints = []
         bool_contains = [False for i in range(-1023, 1024)]
         with open(input_file, 'r') as f:
             for i in f:
-                i = self.trim(i)
+                i = UniqueInt.trim(i)
                 if len(i) == 0:
                     continue
-                num = self.str_to_int(i)
+                num = UniqueInt.str_to_int(i)
                 if num is False:
                     continue
                 if bool_contains[num] == False:
-                    self.custom_append(unique_ints, num)
+                    UniqueInt.custom_append(unique_ints, num)
                     bool_contains[num] = True
 
         with open(output_file, 'w') as f:
-            for index, i in enumerate(self.custom_sort(unique_ints)):
+            for index, i in enumerate(UniqueInt.custom_sort(unique_ints)):
                 if index == len(unique_ints) - 1:
-                    f.write(self.int_to_str(i))
+                    f.write(UniqueInt.int_to_str(i))
                 else:
-                    f.write(self.int_to_str(i) + '\n')
+                    f.write(UniqueInt.int_to_str(i) + '\n')
+
 
 if __name__ == "__main__":
     input_file = input("Enter the path of the input file: ")
